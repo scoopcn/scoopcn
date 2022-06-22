1c00b9bed7666dde12d028171c72e1c144462616
liuxy <391861737@qq.com>
2022-06-22 15:46:05 +0800

    refactor: Adopt the ScoopInstaller's actions

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,8 +1,5 @@
-<#
-.SYNOPSIS
-    Execute Pester tests in repository root directory.
-#>
+#Requires -Modules @{ ModuleName = 'Pester'; MaximumVersion = '4.99' }
 
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $result = Invoke-Pester "$PSScriptRoot\.." -PassThru
-
 exit $result.FailedCount
